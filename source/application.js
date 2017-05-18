4ad799e452f563e13c42a20b4e68336908e2a64a
k.takahashi <tk02dr@gmail.com>
2017-05-18 16:08:04 +0900

    api実用開始

--- source/application.js
+++ source/application.js
@@ -1,13 +1,10 @@
 $(function(){
   kongregateAPI.loadAPI(function(){
     window.kongregate = kongregateAPI.getAPI();
-    // You can now access the Kongregate API with:
-    // kongregate.services.getUsername(), etc
-    // Proceed with loading your game...
+    if(kongregate.services.isGuest()){
+      alert("ok");
+    }
   });
-  if(kongregate.services.isGuest()){
-    alert("ok");
-  }
   var candy = 0;
   $(".candy_number").text(candy);
   $(".center_candy").on("click", function(){
